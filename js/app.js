c511600f00700827fb03c14921bf1097ec2544e0
Omat borje <omatborje@gmail.com>
2021-07-28 07:37:31 +0800

    done with simple email validation on javascript.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const btnEmail = document.querySelector('#btn-email');
+const inputEmail = document.getElementById('email');
+const error = document.querySelector('.error');
+
+
+function emailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+btnEmail.addEventListener('click', (e) => {
+    let inputValue = inputEmail.value;
+    e.preventDefault;
+    if (!emailIsValid(inputValue)) {
+        error.style.display = "block";
+    } else {
+        error.style.display = "none";
+        inputEmail.value = "";
+        alert('email is valid')
+    }
+})
